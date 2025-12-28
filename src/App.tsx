931ebad9966ed225b3f6de056c58fdbbb6a35d1e
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-12-28 18:12:30 +0200

    add src & public folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { HomePage } from './components/HomePage/HomePage';
+import { Header } from './components/Header/Header';
+import { PhonesPage } from './components/PhonesPage/PhonesPage';
+import { Footer } from './components/Footer/Footer';
+import { TabletsPage } from './components/TabletsPage/TabletsPage';
+import { AccessoriesPage } from './components/AccessoriesPage/AccessoriesPage';
+// eslint-disable-next-line
+import { ProductDetailsPage } from './components/ProductDetailsPage/ProductDetailsPage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { FavoritesPage } from './components/FavoritesPage/FavoritesPage';
+import { CartPage } from './components/CartPage/CartPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => (
+  <div className="App">
+    <h1 className="hidden">Product Catalog</h1>
+    <Header />
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+    <main className="App__content">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+        <Route path="/phones">
+          <Route index element={<PhonesPage />} />
+          <Route path=":productId" element={<ProductDetailsPage />} />
+        </Route>
+        <Route path="/tablets">
+          <Route index element={<TabletsPage />} />
+          <Route path=":productId" element={<ProductDetailsPage />} />
+        </Route>
+        <Route path="/accessories">
+          <Route index element={<AccessoriesPage />} />
+          <Route path=":productId" element={<ProductDetailsPage />} />
+        </Route>
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </main>
+
+    <Footer />
+  </div>
+);
